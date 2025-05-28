f960d4a3be77a55226bb2e36c3457a59bf56e100
Atilaac <achraf.atila@gmail.com>
2025-05-29 00:11:09 +0200

    Added analysis function
    - The neighbour list is constructed using Cell-list algorithm which have O(N) scalling.
    - Added script to analyze coordination environments and network structure from a LAMMPS dump file. Works also for multicomponant glasses.
    - can get Oxygen coordination (O_n) with respect to network formers.
    - Coordination of each network former with oxygen.
    - Coordination of each network modifier with oxygen.
    - Qn distribution (number of bridging oxygens around Si).
    - a fuction that prints the computed distributions (O_n, <former>_n, <modifier>_n, Qn) to output files named accordingly.
    - function to get the bond angle distribution.

--- pyiron_glass/src/pyiron_glass/__init__.py
+++ pyiron_glass/src/pyiron_glass/__init__.py
@@ -1,11 +1,33 @@
 from pyiron_glass.meltquench import melt_quench_simulation
 from pyiron_glass.potential import generate_potential
 from pyiron_glass.structure import get_ase_structure, get_structure_dict
+from pyiron_glass.structure_analysis import (
+    read_lammps_dump,
+    remove_atom_type,
+    compute_cell_list,
+    get_neighbors,
+    count_distribution,
+    compute_coordination,
+    compute_Qn,
+    compute_network_connectivity,
+    write_distribution_to_file,
+    compute_angles,
+    write_angle_distribution,
 
+)
 
 __all__ = [
     "melt_quench_simulation",
     "generate_potential",
     "get_ase_structure",
     "get_structure_dict",
+    "read_lammps_dump",
+    "compute_coordination",
+    "compute_Qn",
+    "compute_network_connectivity",
+    "write_distribution_to_file",
+    "compute_angles",
+    "write_angle_distribution",
+
+
 ]
