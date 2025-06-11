f03e33ece914d6b440ea612f4e7c35a993370e3a
Leopold Talirz <leopold.talirz@gmail.com>
2025-06-11 22:23:55 +0200

    chore: reprodicible atoms generation
    test that structure generation is reproducible given the random seed.

---
+++ pyiron_glass/src/tests/test_structure.py
@@ -0,0 +1,34 @@
+"""Testing structure generation."""
+
+import pyiron_glass.structure as ps
+
+
+def test_reproducibility():
+    """Test that the random structure generation is reproducible when using a fixed seed."""
+    seed = 42
+    comp = "0.25CaO-0.25Al2O3-0.5SiO2"
+    n_molecules = 10
+    box_length = 10.0  # Angstrom
+    min_distance = 1.6  # Angstrom
+    max_attempts_per_atom = 100000  # Max attempts to place an atom
+
+    kwargs = dict(
+        n_molecules=n_molecules,
+        box_length=box_length,
+        min_distance=min_distance,
+        max_attempts_per_atom=max_attempts_per_atom,
+        STOICHIOMETRY=ps.extract_stoichiometry(comp),
+        seed=seed,
+    )
+
+    atoms1 = ps.create_random_atoms(comp, **kwargs)
+    atoms2 = ps.create_random_atoms(comp, **kwargs)
+
+    assert atoms1 == atoms2, "Random structures should be identical with the same seed."
+
+    # Test with a different seed
+    seed = 12345
+    kwargs["seed"] = seed
+    atoms3 = ps.create_random_atoms(comp, **kwargs)
+
+    assert atoms1 != atoms3, "Random structures should differ with different seeds."
