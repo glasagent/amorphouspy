c7a1077de24cf919b5df31f5a7d77631a92ec02a
gitdowski <marcel.sadowski@schott.com>
2025-07-02 15:50:57 +0200

    add placeholder for CTE

---
+++ pyiron_glass/src/pyiron_glass/analysis/cte.py
@@ -0,0 +1,18 @@
+from ase import Atoms
+
+def extract_CTE(structure: Atoms) -> float:
+    """Extract the coefficient of thermal expansion (CTE) from the given atomic structure.
+
+    Parameters
+    ----------
+    structure : Atoms
+        The atomic structure from which to extract the CTE.
+
+    Returns
+    -------
+    float
+        The extracted CTE value.
+    """
+
+    # Placeholder implementation
+    return 0.0
