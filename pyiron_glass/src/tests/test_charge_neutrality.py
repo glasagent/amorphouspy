36cf12edc348a22524461b67144a8e9ae2b7f12e
Atilaac <achraf.atila@gmail.com>
2025-08-13 19:03:35 +0200

    Update test_charge_neutrality.py

--- pyiron_glass/src/tests/test_charge_neutrality.py
+++ pyiron_glass/src/tests/test_charge_neutrality.py
@@ -21,9 +21,3 @@ def test_check_neutral_oxide_nao2() -> None:
         check_neutral_oxide("NaO2")
     assert "Cannot determine oxidation states" in str(excinfo.value)
 
-
-def test_check_neutral_oxide_no_oxidation_states() -> None:
-    """Test that formulas without oxidation state guesses raise appropriate errors."""
-    with pytest.raises(ValueError, match="'ABC' is not a valid Element") as excinfo:
-        check_neutral_oxide("ABC")
-    assert "is not a valid Element" in str(excinfo.value)
