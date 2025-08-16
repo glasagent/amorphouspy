595e8783ca45b412e032145c6a3597195720c650
Achraf Atila <achraf.atila@gmail.com>
2025-08-16 11:40:11 +0200

    Added initial density approximation from the Fluegel model (#55)
    * Added initial density approximation from the Fluegel model
    
    * Update pyproject.toml to 0.0.2

--- pyiron_glass/src/pyiron_glass/__init__.py
+++ pyiron_glass/src/pyiron_glass/__init__.py
@@ -17,11 +17,17 @@ from pyiron_glass.neighbors import get_neighbors
 from pyiron_glass.potentials import potential
 from pyiron_glass.potentials.potential import generate_potential
 from pyiron_glass.shared import count_distribution, type_to_dict
-from pyiron_glass.structure import get_ase_structure, get_structure_dict
+from pyiron_glass.structure import (
+    check_neutral_oxide,
+    get_ase_structure,
+    get_glass_density_from_model,
+    get_structure_dict,
+)
 from pyiron_glass.workflows.meltquench import melt_quench_simulation
 from pyiron_glass.workflows.viscosity import viscosity_simulation
 
 __all__ = [
+    "check_neutral_oxide",
     "compute_angles",
     "compute_cavities",
     "compute_coordination",
@@ -33,6 +39,7 @@ __all__ = [
     "generate_bond_length_dict",
     "generate_potential",
     "get_ase_structure",
+    "get_glass_density_from_model",
     "get_neighbors",
     "get_properties_for_structure_analysis",
     "get_structure_dict",
