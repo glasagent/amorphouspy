10da6abb5fd74f2cd26c0ef947c7319bcefd2de3
Jan Janßen <janssen@mpie.de>
2025-11-29 14:19:39 +0100

    fixes

--- pyiron_glass/src/pyiron_glass/workflows/md.py
+++ pyiron_glass/src/pyiron_glass/workflows/md.py
@@ -74,11 +74,13 @@ def _run_lammps_md(
     """
     lmp_command = "mpiexec -n 1 --oversubscribe lmp_mpi -in lmp.in"
     if server_kwargs is not None:
-        if isinstance(server_kwargs, dict) and len(server_kwargs) > 1:
+        if isinstance(server_kwargs, dict) and len(server_kwargs) == 1:
             if "cores" in server_kwargs:
                 lmp_command = "mpiexec -n {} --oversubscribe lmp_mpi -in lmp.in".format(str(server_kwargs["cores"]))
             else:
                 raise ValueError
+        elif isinstance(server_kwargs, dict) and len(server_kwargs) == 0:
+            pass
         else:
             raise ValueError
 
